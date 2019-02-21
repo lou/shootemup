684f10967e4d828ba09c79c6d402404a53325c18
lou <louiscuny@gmail.com>
2019-02-21 16:47:08 +0100

    Weapon events done right

--- src/sprites/Weapon.js
+++ src/sprites/Weapon.js
@@ -6,39 +6,34 @@ export default class Weapon extends Phaser.GameObjects.Zone {
     this.scene = scene
     scene.add.existing(this)
     scene.physics.world.enable(this)
-    this.lastFired = 0
     this.type = options.type || 'bullets'
     this.fireSpeed = options.fireSpeed || 1000
     this.speed = options.speed || 200
     this.detectionRange = options.detectionRange || 500
     this.lifespan = options.lifespan
-  }
+    scene.time.addEvent({
+      delay: this.fireSpeed,
+      callback: () => {
+        const distanceOfPlayer = Phaser.Math.Distance.Between(this.x, this.y, scene.player.x, scene.player.y)
 
-  fire(time) {
-    if (time - this.lastFired > this.fireSpeed) {
-      const { player } = this.scene
-      const bullet = this.scene[this.type].get().setVisible(true).setActive(true)
-
-      if (bullet) {
-        bullet.fire(
-          { x: this.x, y: this.y, offset: 30, rotation: this.rotation },
-          { x: player.x, y: player.y },
-          { speed: this.speed, lifespan: this.lifespan }
-        )
-      }
-      this.lastFired = time
-    }
-  }
-
-  preUpdate() {
-    const distanceOfPlayer = Phaser.Math.Distance.Between(this.x, this.y, this.scene.player.x, this.scene.player.y)
-
-    if (this.active && distanceOfPlayer <= this.detectionRange) {
-      this.scene.time.delayedCall(0, () => {
-        if (this.scene) {
-          this.fire(this.scene.time.now)
+        if (this.active && distanceOfPlayer <= this.detectionRange) {
+          this.fire(scene.time.now)
         }
-      })
+      },
+      loop: true
+    })
+  }
+
+  fire() {
+    const { player } = this.scene
+    const bullet = this.scene[this.type].get().setVisible(true).setActive(true)
+
+    if (bullet) {
+      bullet.fire(
+        { x: this.x, y: this.y, offset: 30, rotation: this.rotation },
+        { x: player.x, y: player.y },
+        { speed: this.speed, lifespan: this.lifespan }
+      )
     }
   }
 }
--- src/sprites/projectiles/ExplosiveBullet.js
+++ src/sprites/projectiles/ExplosiveBullet.js
@@ -4,7 +4,7 @@ export default class ExplosiveBullet extends Projectile {
   constructor(scene) {
     super(scene, 'bullet', {
       force: 1,
-      lifespan: { min: 1000, max: 4000 },
+      lifespan: { min: 1000, max: 5000 },
       explosive: true
     })
     this.setScale(0.9)

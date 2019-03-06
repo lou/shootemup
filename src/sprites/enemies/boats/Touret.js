1879b2b3eba839b52ee7fba74a6f5382e3e9c22e
lou <louiscuny@gmail.com>
2019-03-06 15:45:26 +0100

    Add smoke on touret killed

--- src/sprites/enemies/boats/Touret.js
+++ src/sprites/enemies/boats/Touret.js
@@ -16,6 +16,27 @@ export default class Touret extends Phaser.Physics.Arcade.Image {
       .setScale(1.3)
     this.rotatable = true
     this.weapon = new Weapon(scene, { ...options, lifespan: 500 })
+    this.smokeParticles = this.scene.add.particles('smoke')
+    this.smokeEmitter = this.smokeParticles.setDepth(2.2).createEmitter({
+      name: 'smoke',
+      blendMode: 'SCREEN',
+      scale: { start: 0.1, end: 0.3 },
+      speed: 10,
+      rotate: { min: -90, max: 90 },
+      lifespan: 3000,
+      alpha: { start: 0.15, end: 0 },
+      maxParticles: 1,
+      radial: true,
+      tint: 0xfffbde,
+      on: false,
+      follow: this,
+      frequency: 50,
+      maxParticles: 1,
+      tint: 0x65afe3
+    }).reserve(100)
+    this.hitParticles = this.scene.add.particles('hit').setDepth(1.6)
+    this.hitEmitter = this.hitParticles.createEmitter(hitConfig)
+
   }
 
   preUpdate() {
@@ -25,17 +46,20 @@ export default class Touret extends Phaser.Physics.Arcade.Image {
       this.setActive(false)
       this.weapon.setActive(false)
       this.scene.events.emit('addScore', this.points)
+      this.smokeEmitter.start()
+      this.hitParticles.destroy()
     } else {
       this.setRotation(
         Phaser.Math.Angle.Between(player.x, player.y, this.x, this.y) + Math.PI/2
       )
+      this.weapon.x = this.x
+      this.weapon.y = this.y
     }
-    this.weapon.x = this.x
-    this.weapon.y = this.y
   }
 
   destroy() {
     this.weapon.destroy()
+    this.smokeParticles.destroy()
     super.destroy()
   }
 }
